734e6e982162f09dcac6d359adbd5e9e826925c5
Buddhi Pathirane <2478087+bpathirane@users.noreply.github.com>
2023-10-15 19:22:05 -0400

    Motion detector step one

--- sketch_oct15a/sketch_oct15a.ino
+++ sketch_oct15a/sketch_oct15a.ino
@@ -1,26 +1,29 @@
 
 const int DELAY = 60;
 const int LED_PIN = 9;
-const int BUTTON_PIN = 2;
+const int MOTION_DETECTOR_PIN = 2;
 
 int brightness = 0;
 int fadeAmount = 5;
 
-int lastDebounceTime = 0;
+unsigned long lastDebounceTime = 0;
 int debounceDelay = 50;
 int ledState = HIGH;
 int buttonState = HIGH;
 int lastButtonState = LOW;
+bool motionDetected = false;
+unsigned long lastMotionDetectedTime = 0;
+
+unsigned long activatedDuration = 10 * 1000;
+int fadingDuration = 1 * 1000;
 
 void setup() {
+ Serial.begin(9600);
   // put your setup code here, to run once:
  pinMode(LED_PIN, OUTPUT);
- pinMode(BUTTON_PIN, INPUT);
+ pinMode(MOTION_DETECTOR_PIN, INPUT);
 
- digitalWrite(LED_PIN, buttonState);
- digitalWrite(13, LOW);
-
- Serial.begin(9600);
+ analogWrite(LED_PIN, LOW);
 }
 
 void loop() {
@@ -61,20 +64,43 @@ void loop() {
   // //     Serial.println(buttonState);
   // //   }
   // // }
-  int reading = digitalRead(BUTTON_PIN);
-  if (reading != lastButtonState) {
-    lastDebounceTime = millis();
+  int reading = digitalRead(MOTION_DETECTOR_PIN);
+  // Serial.println(reading);
+  if (reading == HIGH) {
+    lastMotionDetectedTime = lastDebounceTime = millis();
+    Serial.println(lastMotionDetectedTime);
   }
-
+/*
   if ((millis() - lastDebounceTime) > debounceDelay){
+    if (reading == HIGH && !motionDetected) {
+      motionDetected = true;
+      lastMotionDetectedTime = millis();
+      Serial.println(lastMotionDetectedTime);
+      // Serial.println();
+    }
     buttonState = reading;
+  }
+  else {
+    motionDetected = false;
+  }
+  */
+
+  if ((millis() - lastMotionDetectedTime) < activatedDuration) {
+    // Within the activation period
+    brightness = 255;
+    Serial.println((millis() - lastMotionDetectedTime));
+  }
+  else
+  {
+    motionDetected = false;
+    brightness = 0;
   }
 
   // buttonState = digitalRead(BUTTON_PIN);
+  // Serial.println(brightness);
+  // Serial.println(((millis() - lastMotionDetectedTime) > activatedDuration) == true);
 
-  digitalWrite(LED_PIN, buttonState);
+  analogWrite(LED_PIN, brightness);
   // delay(debounceDelay);
   // buttonState = !buttonState;
-
-  lastButtonState = reading;
 }
